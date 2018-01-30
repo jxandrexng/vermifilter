0b0f45d04398d9719fb07db879f368c0e6ac9030
jxandrexng <jxandrexng@gmail.com>
2018-01-31 03:27:16 +0800

    Added more functions and logic

--- main/main.ino
+++ main/main.ino
@@ -1,118 +1,116 @@
 #include <EEPROM.h>
 #include <NewPing.h> //Ultrasonic Sensor Library
-#include <AnalogPHMeter.h>
-#include <Servo.h> 
+#include <Servo.h>
 
 // CONSTANTS
 #define TRIGGER1_PIN  12  // Arduino pin tied to trigger pin on the first ultrasonic sensor.
 #define ECHO1_PIN     11  // Arduino pin tied to echo pin on the first ultrasonic sensor.
 #define TRIGGER2_PIN  10  // Arduino pin tied to trigger pin on the second ultrasonic sensor.
 #define ECHO2_PIN     9  // Arduino pin tied to echo pin on the second ultrasonic sensor.
-#define RELAY1       3  // Arduino pin tied to relay1 pin of the first pump.
-#define RELAY2       4  // Arduino pin tied to relay2 pin of the second pump.
-#define RELAY3       5  // Arduino pin tied to relay3 pin of the first pump.
+#define RELAY1       3  // Arduino pin tied to relay1 pin of the influent pump.
+#define RELAY2       4  // Arduino pin tied to relay2 pin of the washer1 pump.
+#define RELAY3       5  // Arduino pin tied to relay3 pin of the washer2 pump.
 #define RELAY4       6  // Arduino pin tied to relay4 pin of the second pump.
 #define TURBIDITY1_PIN A0 // Arduino pin tied to turbidity1 sensor.
 #define TURBIDITY2_PIN A1 // Arduino pin tied to turbidity2 sensor.
 #define PH_SENSOR1_PIN A2 // Arduino pin tied to ph1 sensor.
 #define PH_SENSOR2_PIN A3 // Arduino pin tied to ph2 sensor.
 #define MAX_DISTANCE 500 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define waterLimit 10 //Limit in cm to which the ultrasonic sensor is in proximity to the water.
+#define containerHeight 30 //Height of the container in cm
+#define Offset1 0.00  //Deviation compensate for pH Sensor1
+#define Offset2 0.00  //Deviation compensate for pH Sensor2
+#define ArrayLength  40 //Times of collection
+
 
 
 // DECLARATION
-AnalogPHMeter pHSensor1(PH_SENSOR1_PIN);
-AnalogPHMeter pHSensor2(PH_SENSOR2_PIN);
-unsigned int pHCalibrationValueAddress = 0;
-int turbidityUnit1 = 0, turbidityUnit2 = 0;
+int pHArray[ArrayLength];   //Store the average value of the sensor feedback
+int pHArrayIndex = 0;
 int distance1, distance2, pos = 0;
-Servo myservo1, myservo2, myservo3, myservo4;
+int turbidityUnit1 = 0, turbidityUnit2 = 0;
+unsigned int pHCalibrationValueAddress = 0;
+float pHUnit1 = 0, pHUnit2 = 0;
+Servo myservo1, myservo2;
 NewPing sonar1(TRIGGER1_PIN, ECHO1_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance. Ultrasonic Sensor NewPing
 NewPing sonar2(TRIGGER2_PIN, ECHO2_PIN, MAX_DISTANCE);
 
 // SETUP
 void setup() {
-  struct PHCalibrationValue pHCalibrationValue;
   Serial.begin(9600); // Open serial monitor at 9600 baud to see ping results
-  EEPROM.get(pHCalibrationValueAddress, pHCalibrationValue);
-  pHSensor1.initialize(pHCalibrationValue);
-  pHSensor2.initialize(pHCalibrationValue);
   pinMode(RELAY1, OUTPUT);
   pinMode(RELAY2, OUTPUT);
   pinMode(RELAY3, OUTPUT);
   pinMode(RELAY4, OUTPUT);
-  pinMode(22, OUTPUT);
-  digitalWrite(22, HIGH); 
-  pinMode(24, OUTPUT);
-  digitalWrite(24, HIGH);  
-  myservo1.attach(44);
-//  myservo2.attach(45);
-//  myservo3.attach(46);
-//  myservo4.attach(47);
+  myservo1.attach(44); //Attach servo1 to pin 44.
+  myservo2.attach(46); //Attach servo2 to pin 46.
+  relay1_On(); //Pump water to influent container.
+  relay2_On(); //Pump cleaning water to pH sensor.
 }
 
 // LOOP
 void loop() {
-  pH1_Read();
   pH1_Print();
-  turbidity1_Read();
   turbidity1_Print();
-  distance1_Read();
-  distance1_Print();
-  Serial.print(" | ");
-  pH2_Read();
+  volume1_Print();
   pH2_Print();
-  turbidity2_Read();
   turbidity2_Print();
-  distance2_Read();
-  distance2_Print();
+  volume2_Print();
+  pump_Influent();
+  pump_Effluent();
   Serial.print("\n");
-  relay1_On();
-  relay2_On();
-  relay1_Off();
-  relay2_Off();
-//  servo1_Expand();
-//  servo1_Retract();
-  delay(1000);
 }
 
-int distance1_Read(){
-   distance1 = sonar1.ping_cm();
-   return distance1;
+int distance1_Read() {
+  distance1 = sonar1.ping_cm();
+  return distance1;
 }
 
 void distance1_Print() {
+  Serial.print("Distance1: ");
   Serial.print(distance1); // Send ping, get distance in cm and print result (0 = outside set distance range)
   Serial.print(" ");
 }
 
-int distance2_Read(){
-   distance2 = sonar2.ping_cm();
-   return distance2;
+int distance2_Read() {
+  distance2 = sonar2.ping_cm();
+  return distance2;
 }
 
 void distance2_Print() {
+  Serial.print("Distance2: ");
   Serial.print(distance2); // Send ping, get distance in cm and print result (0 = outside set distance range)
   Serial.print(" ");
 }
 
-void pH1_Read() {
-  pHSensor1.calibrationClear();
-  EEPROM.put(pHCalibrationValueAddress, pHSensor1.getCalibrationValue());
+float pH1_Read() {
+  static float voltage;
+  pHArray[pHArrayIndex++] = analogRead(PH_SENSOR1_PIN);
+  if (pHArrayIndex == ArrayLength)pHArrayIndex = 0;
+  voltage = avergearray(pHArray, ArrayLength) * 5.0 / 1024;
+  pHUnit1 = 3.5 * voltage + Offset1;
+  return pHUnit1;
 }
 
-void pH1_Print(){
-  Serial.print(pHSensor1.singleReading().getpH());
-  Serial.print(" ");
+void pH1_Print() {
+  pH1_Read();
+  Serial.print(pHUnit1);
+  Serial.print("pH ");
 }
 
-void pH2_Read() {
-  pHSensor2.calibrationClear();
-  EEPROM.put(pHCalibrationValueAddress, pHSensor2.getCalibrationValue());
+float pH2_Read() {
+  static float voltage;
+  pHArray[pHArrayIndex++] = analogRead(PH_SENSOR2_PIN);
+  if (pHArrayIndex == ArrayLength)pHArrayIndex = 0;
+  voltage = avergearray(pHArray, ArrayLength) * 5.0 / 1024;
+  pHUnit2 = 3.5 * voltage + Offset2;
+  return pHUnit2;
 }
 
-void pH2_Print(){
-  Serial.print(pHSensor2.singleReading().getpH());
-  Serial.print(" ");
+void pH2_Print() {
+  pH2_Read();
+  Serial.print(pHUnit2);
+  Serial.print("pH ");
 }
 
 void turbidity1_Read() {
@@ -121,9 +119,10 @@ void turbidity1_Read() {
   turbidityUnit1 = sensorValue;
 }
 
-void turbidity1_Print(){
-    Serial.print(turbidityUnit1);
-    Serial.print(" ");
+void turbidity1_Print() {
+  turbidity1_Read();
+  Serial.print(turbidityUnit1);
+  Serial.print("NTU ");
 }
 
 void turbidity2_Read() {
@@ -132,9 +131,22 @@ void turbidity2_Read() {
   turbidityUnit2 = sensorValue;
 }
 
-void turbidity2_Print(){
-    Serial.print(turbidityUnit2);
-    Serial.print(" ");
+void turbidity2_Print() {
+  turbidity2_Read();
+  Serial.print(turbidityUnit2);
+  Serial.print("NTU ");
+}
+
+void volume1_Print() {
+  float waterVolume1 = ((30.0 - distance1) * (30.0 * 40.0) / (30.0 * 30.0 * 40.0)) * 100.0;
+  Serial.print(waterVolume1);
+  Serial.print("% ");
+}
+
+void volume2_Print() {
+  float waterVolume2 = ((30.0 - distance2) * (30.0 * 40.0) / (30.0 * 30.0 * 40.0)) * 100.0;
+  Serial.print(waterVolume2);
+  Serial.print("% ");
 }
 
 void relay1_On() {
@@ -169,77 +181,92 @@ void relay4_Off() {
   digitalWrite(RELAY4, LOW);
 }
 
-void servo1_Expand(){
-    for(pos=0;pos<=90;pos+=1){
-    myservo1.write(pos);
-    delay(5);
-    }
+void servo1_Expand() {
+  myservo1.write(90);
 }
 
-void servo1_Retract(){
-    for(pos=90;pos>=1;pos-=1){
-    myservo1.write(pos);
-    delay(5);
-    }
+void servo1_Retract() {
+  myservo1.write(0);
 }
 
-void servo2_Expand(){
-    for(pos=0;pos<=90;pos+=1){
-    myservo2.write(pos);
-    delay(5);
-    }
-    delay(5000);
+void servo2_Expand() {
+  myservo2.write(90);
 }
 
-void servo2_Retract(){
-    for(pos=90;pos>=1;pos-=1){
-    myservo2.write(pos);
-    delay(5);
-    }
-    delay(5000);
+void servo2_Retract() {
+  myservo2.write(0);
 }
 
-void servo3_Expand(){
-    for(pos=0;pos<=90;pos+=1){
-    myservo3.write(pos);
-    delay(5);
-    }
-    delay(5000);
-}
-
-void servo3_Retract(){
-    for(pos=90;pos>=1;pos-=1){
-    myservo3.write(pos);
-    delay(5);
-    }
-    delay(5000);
-}
-
-void servo4_Expand(){
-    for(pos=0;pos<=90;pos+=1){
-    myservo4.write(pos);
-    delay(5);
-    }
-    delay(5000);
-}
-
-void servo4_Retract(){
-    for(pos=90;pos>=1;pos-=1){
-    myservo4.write(pos);
-    delay(5);
-    }
-    delay(5000);
-}
-
-/*void main(){
-  if(distance < 10){
-    Relay1_On();
-    Servo1_Expand();
+void pump_Influent() {
+  distance1_Read();
+  if (distance1 <= waterLimit) { //If influent water level is nearly full, execute the following functions:
+    relay1_Off(); //Turn off influent pump.
+    relay2_Off(); //Turn off washer pump.
+    servo1_Expand(); //Rotate pH sensor towards untreated water.
   }
-  else
-  {
-    Relay1_Off();
-    Servo1_Retract();
+  else if (distance1 >= waterLimit && distance1 <= containerHeight) { //If influent water is almost empty, execute the following:
+    servo1_Retract(); //Rotate pH sensor towards cleaning water.
+    relay2_On(); //Turn on washer pump.
+    relay1_On(); //Turn on influent pump.
   }
-}*/
+}
 
+void pump_Effluent() {
+  distance2_Read();
+  if (distance2 <= waterLimit) { //If effluent water level is nearly full, execute the following:
+    relay3_Off(); //Turn off washer pump.
+    servo2_Expand(); //Rotate pH sensor towards treated water.
+  }
+  else if (distance2 >= waterLimit && distance2 <= containerHeight) { //If water level is almost empty, execute the following.
+    relay3_On(); //Turn on washer pump.
+    servo2_Retract(); //Rotate pH sensor towards cleaning water.
+  }
+}
+
+//void dilute() {
+//  if(pHUnit1 < 6.00){
+//
+//  } else {
+//
+//  }
+//}
+
+//For pH Sensor
+double avergearray(int* arr, int number) {
+  int i;
+  int max, min;
+  double avg;
+  long amount = 0;
+  if (number <= 0) {
+    return 0;
+  }
+  if (number < 5) { //less than 5, calculated directly statistics
+    for (i = 0; i < number; i++) {
+      amount += arr[i];
+    }
+    avg = amount / number;
+    return avg;
+  } else {
+    if (arr[0] < arr[1]) {
+      min = arr[0]; max = arr[1];
+    }
+    else {
+      min = arr[1]; max = arr[0];
+    }
+    for (i = 2; i < number; i++) {
+      if (arr[i] < min) {
+        amount += min;      //arr<min
+        min = arr[i];
+      } else {
+        if (arr[i] > max) {
+          amount += max;  //arr>max
+          max = arr[i];
+        } else {
+          amount += arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount / (number - 2);
+  }//if
+  return avg;
+}
