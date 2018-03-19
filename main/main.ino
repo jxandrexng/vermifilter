0e22d1a9ef2388c8c9eb3d77ac1a5b056695e47f
jxandrexng <jxandrexng@gmail.com>
2018-03-19 22:53:08 +0800

    Edited main sketch

--- main/main.ino
+++ main/main.ino
@@ -9,11 +9,9 @@
 #define ECHO2_PIN        9  // Arduino pin tied to echo pin on the second ultrasonic sensor.
 #define TEMP_PIN         8  // Arduino pin tied to temperature sensor.
 #define RELAY1           3  // Arduino pin tied to relay1 pin of the influent pump.
-#define RELAY2           4  // Arduino pin tied to relay2 pin of the washer1 pump.
-#define RELAY3           5  // Arduino pin tied to relay3 pin of the washer2 pump.
-#define RELAY4           6  // Arduino pin tied to relay4 pin of the vermibed pump.
-#define RELAY5           7  // Arduino pin tied to relay5 pin of the soda ash pump.
-#define LEVEL_PIN       22  // Arduino pin tied to level pin of the soda ash container.
+#define RELAY2           4  // Arduino pin tied to relay2 pin of the vermibed pump.
+#define RELAY3           5  // Arduino pin tied to relay3 pin of the soda ash pump.
+#define RELAY4           6  // Arduino pin tied to relay4 pin of the mixing pump.
 #define TURBIDITY1_PIN  A0 // Arduino pin tied to turbidity1 sensor.
 #define TURBIDITY2_PIN  A1 // Arduino pin tied to turbidity2 sensor.
 #define PH_SENSOR1_PIN  A2 // Arduino pin tied to ph1 sensor.
@@ -30,21 +28,17 @@
 
 
 // DECLARATION
-int sodaLevel; //Store the binary value for soda ash level
 boolean initStatus = 0; //Store initialization status
+boolean relay2_Status = 0; //Store relay2 status
+char incoming; //Incoming value for serial
+float turbidityUnit1, turbidityUnit2, pHUnit1, pHUnit2, temperature, pHvoltage1, pHvoltage2;
 int pHArray1[ArrayLength1];   //Store the average value of the sensor feedback
 int pHArrayIndex1 = 0;
 int pHArray2[ArrayLength2];   //Store the average value of the sensor feedback
 int pHArrayIndex2 = 0;
-int distance1, distance2, pos = 0;
-float turbidityUnit1 = 0, turbidityUnit2 = 0;
-int moistureValue = 0;
-char incoming; //Incoming value for serial
-unsigned long interval = 1000, previousMillis = 0;
-float pHUnit1 = 0, pHUnit2 = 0, temperature = 0, pHvoltage1, pHvoltage2;
-boolean vermiFlag = 0, influFlag = 0, effluFlag = 0, dilutFlag = 0, relay4_Status = 0u;
+int distance1, distance2, moistureValue;
+unsigned long interval = 1000, previousMillis = 0, soda_interval = 500;
 OneWire ds(TEMPERATURE_PIN);
-Servo myservo1, myservo2;
 NewPing sonar1(TRIGGER1_PIN, ECHO1_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance. Ultrasonic Sensor NewPing1
 NewPing sonar2(TRIGGER2_PIN, ECHO2_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance. Ultrasonic Sensor NewPing2
 
@@ -55,10 +49,6 @@ void setup() {
   pinMode(RELAY2, OUTPUT); //Set relay2 as output.
   pinMode(RELAY3, OUTPUT); //Set relay3 as output.
   pinMode(RELAY4, OUTPUT); //Set relay4 as output.
-  pinMode(RELAY5, OUTPUT); //Set relay5 as output.
-  pinMode(LEVEL_PIN, INPUT); //Set level pin as input.
-  myservo1.attach(44); //Attach servo1 to pin 44.
-  myservo2.attach(46); //Attach servo2 to pin 46.
 }
 
 // LOOP
@@ -69,10 +59,9 @@ void loop() {
     if (incoming == '1') {
       initStatus = true;
       relay1_On(); //Pump water to influent container.
-      relay2_On(); //Pump cleaning water to pH input sensor.
-      relay3_On(); //Pump cleaning water to pH output sensor.
-      relay4_Off(); //Standby for vermibed pump.
-      relay5_Off(); //Standby for soda ash pump.
+      relay2_Off(); //Standby for vermibed pump.
+      relay3_Off(); //Standby for soda ash pump.
+      relay4_Off(); //Standby for mixing pump.
       while (initStatus) {
         mainF();
         main_printF();
@@ -83,7 +72,6 @@ void loop() {
           digitalWrite(RELAY2, LOW);
           digitalWrite(RELAY3, LOW);
           digitalWrite(RELAY4, LOW);
-          digitalWrite(RELAY5, LOW);
         }
       }
     }
@@ -101,17 +89,14 @@ void main_printF() {
     volume2_Print();
     temp_Print();
     moisture_Print();
-    soda_Print();
     Serial.print("\n");
     previousMillis = currentMillis;
   }
 }
 
 void mainF() {
-  pump_Effluent();
-  pump_Vermibed();
-  pump_Soda();
   pump_Influent();
+  pump_Vermibed();
 }
 
 int distance1_Read() {
@@ -301,103 +286,52 @@ void relay4_Off() {
   digitalWrite(RELAY4, LOW);
 }
 
-void relay5_On() {
-  digitalWrite(RELAY5, HIGH);
-}
-
-void relay5_Off() {
-  digitalWrite(RELAY5, LOW);
-}
-
-void servo1_Expand() {
-  myservo1.write(90);
-}
-
-void servo1_Retract() {
-  myservo1.write(0);
-}
-
-void servo2_Expand() {
-  myservo2.write(90);
-}
-
-void servo2_Retract() {
-  myservo2.write(0);
-}
-
-void soda_Print() {
-  sodaLevel = digitalRead(LEVEL_PIN);
-  if (sodaLevel == 1) {
-    Serial.print("OKAY");
-  } else {
-    Serial.print("LOW");
-  }
-
-}
-
-void pump_Soda() {
-  sodaLevel = digitalRead(LEVEL_PIN);
-  if (sodaLevel = HIGH) {
-    relay5_On();
-  } else {
-    relay5_Off();
-  }
-}
-
 void pump_Influent() {
   distance1_Read();
   if (distance1 <= waterLimit) { //If influent water level is nearly full, execute the following functions:
     relay1_Off(); //Turn off influent pump.
-    relay2_Off(); //Turn off washer pump.
-    servo1_Expand(); //Rotate pH sensor towards untreated water.
   }
   else if (distance1 >= waterLimit && distance1 <= containerHeight) { //If influent water is almost empty, execute the following:
-    servo1_Retract(); //Rotate pH sensor towards cleaning water.
-    relay2_On(); //Turn on washer pump.
     relay1_On(); //Turn on influent pump.
   }
 }
 
 void pump_Vermibed() {
   distance1_Read();
-  //Do logic that pumps water to vermibed while influent container is not empty without disrupting other functions.
-  unsigned long currentMillis = millis();
-  if (distance1 == containerHeight) {
-    if (currentMillis - previousMillis >= interval && !relay4_Status) {
-      relay4_On(); //Turn on vermibed pump.
-      previousMillis = currentMillis;
-      relay4_Status = 1;
-    }
-    else if (currentMillis - previousMillis >= interval && relay4_Status) {
-      relay4_Off();
-      previousMillis = currentMillis;
-      relay4_Status = 0;
-    }
-  }
-  //Add timer interval that reads soil moisture.
-  if (distance1 == containerHeight) {
-    relay4_Off();
-  }
-}
-
-void pump_Effluent() {
-  distance2_Read();
-  if (distance2 <= waterLimit) { //If effluent water level is nearly full, execute the following:
-    relay3_Off(); //Turn off washer pump.
-    servo2_Expand(); //Rotate pH sensor towards treated water.
-  }
-  else if (distance2 >= waterLimit && distance2 <= containerHeight) { //If water level is almost empty, execute the following.
-    relay3_On(); //Turn on washer pump.
-    servo2_Retract(); //Rotate pH sensor towards cleaning water.
-  }
-}
-
-void dilute() {
   pH1_Read();
-  if (pHUnit1 < 6.00) {
-    relay5_On();
+  unsigned long currentMillis = millis();
+  if(pHUnit1 < 6.00) {
+    pump_Soda();
+  }
+  else if (distance1 == containerHeight) {
+    if (currentMillis - previousMillis >= interval && !relay2_Status) {
+      relay2_On(); //Turn on vermibed pump.
+      previousMillis = currentMillis;
+      relay2_Status = 1;
+    }
+    else if (currentMillis - previousMillis >= interval && relay2_Status) {
+      relay2_Off();
+      previousMillis = currentMillis;
+      relay2_Status = 0;
+    }
+  }
+}
+
+void pump_Soda() {
+  pH1_Read();
+  if (pHUnit1 < 6.00) { // If pH reading is less than 6pH, pump soda ash to dilute input
+    unsigned long currentMillis = millis();
+    if(currentMillis - previousMillis >= soda_interval){
+      relay3_On();
+      previousMillis = currentMillis;
+    }
+    if(currentMillis - previousMillis >= interval){
+      relay4_On();
+      previousMillis = currentMillis;
+    }
   } else {
-    relay5_Off();
+    relay3_Off();
+    relay4_Off();
   }
 }
 
